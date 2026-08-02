35221a04dd0fb4514475234c23c2c68b431adcaa
Khushi Grewal <khushibca999@gmail.com>
2026-08-02 19:01:48 +0530

    Created Welcome Screen

---
+++ portfolio evidence/Mini Portfolio Projects/To-do app/main.py
@@ -0,0 +1,21 @@
+##Mini Portfolio project - To-Do App
+
+# ---------------Welcome Screen------------------------
+
+print("""
+==========================================
+           TO-DO APP
+==========================================
+
+Welcome!
+
+Organize your daily tasks efficiently.
+
+Version : 1.0 (Console MVP)
+
+------------------------------------------
+Loading application...
+
+""")
+
+
